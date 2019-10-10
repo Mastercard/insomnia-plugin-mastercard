e9e02a5534718d08091a9f967308295ec0327b43
Thompson, Brian <brian.thompson@mastercard.com>
2019-10-09 23:55:10 -0500

    Clean up package.json
    Change author to maintainers to package.json and add maintainers
    Remove empty lines where needed
    Remove core Node.js dependency from package.json; doesn't need to be defined there

--- src/mastercard-auth.js
+++ src/mastercard-auth.js
@@ -11,15 +11,13 @@ module.exports = function (context) {
     context.request.setParameter(entry.name, entry.value.replace(/,/g, "%25252C"));    
   });
 
-  
   const qs = buildQueryStringFromParams(context.request.getParameters());
   const fullUrl = joinUrlAndQueryString(context.request.getUrl(), qs);
   const url = smartEncodeUrl(fullUrl, true);
-
   const mastercard = context.request.getEnvironmentVariable('mastercard');
+
   if (mastercard) {
     try {
-
       const p12Content = fs.readFileSync(mastercard.keystoreP12Path, 'binary');
       const p12Asn1 = forge.asn1.fromDer(p12Content, false);
       const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, mastercard.keystorePassword);
@@ -28,11 +26,9 @@ module.exports = function (context) {
                            bagType: forge.pki.oids.pkcs8ShroudedKeyBag
                             }).friendlyName[0];
       const signingKey = forge.pki.privateKeyToPem(keyObj.key);
-
       const authHeader = oauth.getAuthorizationHeader(URL.parse(url), context.request.getMethod(), context.request.getBodyText(), mastercard.consumerKey, signingKey);
+
       context.request.setHeader('Authorization', authHeader);
-
-
     } catch (err) {
       alert(err.message);
     }
