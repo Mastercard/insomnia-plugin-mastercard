8cb8b30a564a0a0c5e54c504137ac8177de7a950
Ross Phelan <phelanr2@tcd.ie>
2021-03-22 11:37:48 +0000

    Update mastercard-auth.js
    Updating check to attach authorization header only if a Mastercard environment variable is found. Before a Mastercard domain and a Mastercard environment variable were required.

--- src/mastercard-auth.js
+++ src/mastercard-auth.js
@@ -16,7 +16,7 @@ module.exports = function (context) {
 
   const mastercard = context.request.getEnvironmentVariable('mastercard');
 
-  if (mastercard && commaDecodedUrl.includes('mastercard.com')) {
+  if (mastercard) {
     try {
       if(mastercard.keystoreP12Path === defaultKeystoreP12PathSandbox || mastercard.keystoreP12Path === defaultKeystoreP12PathProd){
         throw Error("Please update the keystoreP12Path property from the default in the Mastercard environment settings")
