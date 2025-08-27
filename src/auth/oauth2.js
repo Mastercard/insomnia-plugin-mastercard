432252c1b0e90b49b5111432df38d343d897ff98
Neeraj Joseph Koilparambil <neeraj.josephkoilparambil@mastercard.com>
2025-08-27 09:47:23 +0100

    use last file modified time to determine config change

--- src/auth/oauth2.js
+++ src/auth/oauth2.js
@@ -2,20 +2,21 @@ const { OAuth2 } = require('oauth2-client');
 const MastercardContext = require('../mastercard-context');
 const { extractPrivateKeyFromP12 } = require('./helper');
 const { deepEqual } = require('fast-equals')
+const { stat } = require('fs/promises')
 
 
 // scope the client at the module level so it can make use of the token cache.
 let oAuth2Client;
 let lastUsedConfig;
+let lastUsedP12FileModifiedTime;
 
 async function getAuthorizationHeaders(mcContext, dPopNonce = null) {
   const insomnia = mcContext.insomnia;
   const config = mcContext.config.oAuth2;
 
   // re-instantiate the client if this is the first time or if the config has changed
-  if(!oAuth2Client || hasConfigChanged(lastUsedConfig, config)) {
+  if(!oAuth2Client || await hasConfigChanged(lastUsedConfig, config, lastUsedP12FileModifiedTime)) {
     // config validation has been done by utils/validator.js already
-    lastUsedConfig = config;
     const signingKey = extractPrivateKeyFromP12(config.keystoreP12Path, config.keyAlias, config.keystorePassword)
     oAuth2Client = await OAuth2.clientCredentialsClient({
       keyId: config.keyId,
@@ -26,6 +27,9 @@ async function getAuthorizationHeaders(mcContext, dPopNonce = null) {
       tokenFetchTimeout: config.tokenFetchTimeout,
       tokenCache: config.tokenCache
   })
+
+  lastUsedConfig = config;
+  lastUsedP12FileModifiedTime = await getFileModifiedTime(config.keystoreP12Path)
   }
   return oAuth2Client.getAuthHeaders({
     url: mcContext.url,
@@ -35,31 +39,27 @@ async function getAuthorizationHeaders(mcContext, dPopNonce = null) {
   });
 }
 
-function hasConfigChanged(oldConfig, newConfig) {
-  return !deepEqual(oldConfig, newConfig);
+async function hasConfigChanged(oldConfig, newConfig, lastP12FileModifiedTime) {
+  // config has definitely changed
+  if(!deepEqual(oldConfig, newConfig)) {
+    true
+  }
+
+  // the config itself hasn't changed, so check if the file has been replaced.
+  return lastP12FileModifiedTime !== await getFileModifiedTime(newConfig.keystoreP12Path)
 }
 
-
-module.exports.request = async (context) => {
-  const mcContext = new MastercardContext(context);
-
-  if (mcContext.isMastercardRequest() && mcContext.isOAuth2Request()) {
-    try {
-      
-      const dpopNonce = await fetchNonce(context, mcContext);
-
-      const { Authorization, DPoP } = await getAuthorizationHeaders(mcContext, dpopNonce)
-      context.request.setHeader('Authorization', Authorization);
-      context.request.setHeader('DPoP', DPoP);
-    } catch (err) {
-      if (err.code === 'ENOENT') {
-        err.message = `No P12 file found at location: ${err.path}`;
-      }
-      window.alert(err.message); // eslint-disable-line no-undef
-      throw Error(err);
-    }
+async function getFileModifiedTime(filePath) {
+  try {
+    const stats = await stat(filePath)
+    return stats.mtimeMs
+  } catch(e) {
+    console.error('error fetching file stat')
   }
-};
+
+  // unabel to determine modified time, so return an invalid value
+  return -1;
+}
 
 const fetchNonce = async (context, mcContext) => {
 
@@ -97,3 +97,25 @@ const fetchNonce = async (context, mcContext) => {
 
   return response.headers.get('dpop-nonce')
 }
+
+
+module.exports.request = async (context) => {
+  const mcContext = new MastercardContext(context);
+
+  if (mcContext.isMastercardRequest() && mcContext.isOAuth2Request()) {
+    try {
+      
+      const dpopNonce = await fetchNonce(context, mcContext);
+
+      const { Authorization, DPoP } = await getAuthorizationHeaders(mcContext, dpopNonce)
+      context.request.setHeader('Authorization', Authorization);
+      context.request.setHeader('DPoP', DPoP);
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        err.message = `No P12 file found at location: ${err.path}`;
+      }
+      window.alert(err.message); // eslint-disable-line no-undef
+      throw Error(err);
+    }
+  }
+};
